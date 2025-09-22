55677c21a4d516a783966b47e2008fcf845fe36e
MatthewJonesey <matthew.jones6736@tamu.edu>
2025-09-22 14:50:46 +0000

    1 terminal added

---
+++ client.cpp
@@ -0,0 +1,95 @@
+/*
+	Original author of the starter code
+    Tanzir Ahmed
+    Department of Computer Science & Engineering
+    Texas A&M University
+    Date: 2/8/20
+	
+	Please include your Name, UIN, and the date below
+	Name: Matthew Jones
+	UIN: 533005190
+	Date: 9/19/2025
+*/
+#include "common.h"
+#include "FIFORequestChannel.h"
+#include <sys/wait.h>
+#include <unistd.h>
+
+using namespace std;
+
+
+int main (int argc, char *argv[]) {
+	int opt;
+	int p = 1;
+	double t = 0.0;
+	int e = 1;
+	
+	string filename = "";
+	int buffer = MAX_MESSAGE;
+	while ((opt = getopt(argc, argv, "p:t:e:f:m:")) != -1) {
+		switch (opt) {
+			case 'p':
+				p = atoi (optarg);
+				break;
+			case 't':
+				t = atof (optarg);
+				break;
+			case 'e':
+				e = atoi (optarg);
+				break;
+			case 'f':
+				filename = optarg;
+				break;
+			case 'm':
+				buffer = atoi (optarg);
+				break;
+		}
+	}
+	// Giving arguments for the server: './server', '-m' '<val for -m arg>', 'NULL'
+	// fork
+	// In child run execvp using the server arguments
+	
+	pid_t pid = fork();
+	if(!(pid < 0)){
+			char* args[] = {(char*)"./server", (char*)"-m", (char*)to_string(buffer).c_str(), NULL};
+	
+	if (execvp(args[0], args) < 0) {
+		perror("execvp failed");
+		return 1;
+	}
+	}
+	
+	
+
+    FIFORequestChannel chan("control", FIFORequestChannel::CLIENT_SIDE);
+	
+	// example data point request
+    char buf[MAX_MESSAGE]; // 256
+    datamsg x(1, 0.0, 1);
+	
+	memcpy(buf, &x, sizeof(datamsg));
+	chan.cwrite(buf, sizeof(datamsg)); // question
+	double reply;
+	chan.cread(&reply, sizeof(double)); //answer
+	cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << reply << endl;
+	
+    // sending a non-sense message, you need to change this
+	filemsg fm(0, 0);
+	string fname = "teslkansdlkjflasjdf.dat";
+	
+	int len = sizeof(filemsg) + (fname.size() + 1);
+	char* buf2 = new char[len];
+	memcpy(buf2, &fm, sizeof(filemsg));
+	strcpy(buf2 + sizeof(filemsg), fname.c_str());
+	chan.cwrite(buf2, len);  // I want the file length;
+
+	delete[] buf2;
+	
+	// closing the channel    
+    MESSAGE_TYPE m = QUIT_MSG;
+    chan.cwrite(&m, sizeof(MESSAGE_TYPE));
+
+	waitpid(pid, NULL, 0); // wait for the child process to terminate
+
+	return 0;
+}
